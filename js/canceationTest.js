c459cbde40d82127cb12fc4b0cd8131f2c20d00a
Paulo Figueiro <paulo.figueiro@vibedesenv.com>
2020-01-31 12:32:55 -0300

    adicionando tipo de alvos

---
+++ js/canceationTest.js
@@ -0,0 +1,26 @@
+
+cancelationTest = {
+
+
+ goalTypes = {
+     "carro" : 1,
+     "casa" : 2,
+     "arvore" : 3,
+     "bule" : 4,
+     "sol" : 5,
+     "galinha" : 6,
+     "coelho" : 7,
+     "gato" : 8,
+     "flor" : 9,
+     "peixe" : 10,
+     "estrela" : 11,
+     "aviao" : 12,
+     "barco" : 13,
+ }
+
+
+
+ 
+
+
+}
