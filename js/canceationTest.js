10393e19768b68fd9c1c8310f0b9c9dc6f2e5eb7
Paulo Figueiro <paulo.figueiro@vibedesenv.com>
2020-01-31 14:47:09 -0300

    adicionando estrutura padrão

--- js/canceationTest.js
+++ js/canceationTest.js
@@ -1,26 +1,48 @@
 
-cancelationTest = {
+(function () {
+    
+    /**
+     * @class CancelationTest
+     * @constructor
+     * 
+     * central control of the cancelation test. has the all instances that manages
+     * the test and store the result in the end of defined time
+     */
+    
+    //constructor
+    function CancelationTest(time_seconds, n_goals, n_distractors) 
+    {
+        //public properties
 
 
- goalTypes = {
-     "carro" : 1,
-     "casa" : 2,
-     "arvore" : 3,
-     "bule" : 4,
-     "sol" : 5,
-     "galinha" : 6,
-     "coelho" : 7,
-     "gato" : 8,
-     "flor" : 9,
-     "peixe" : 10,
-     "estrela" : 11,
-     "aviao" : 12,
-     "barco" : 13,
- }
+        //private properties
+        
+        this._time_seconds = time_seconds || 20000;
 
+        this._n_goals = n_goals || 3;
 
+        this._n_distractors = n_distractors || 10;
 
- 
+    }
 
+    //static readonly properties
+    CancelationTest.GOAL_TYPES = [
+        { id : 1, name : "carro" },
+        { id : 2, name : "casa" },
+        { id : 3, name : "arvore" },
+        { id : 4, name : "bule" },
+        { id : 5, name : "sol" },
+        { id : 6, name : "galinha" },
+        { id : 7, name : "coelho" },
+        { id : 8, name : "gato" },
+        { id : 9, name : "flor" },
+        { id : 10, name : "peixe" },
+        { id : 11, name : "estrela" },
+        { id : 12, name : "aviao" },
+        { id : 13, name : "barco" }
+    ];
 
 }
+
+
+)();
