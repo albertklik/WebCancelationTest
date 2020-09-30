d7938d66b85dbf89a25b973feb1646e9626c38f3
Albertklik <paulo.albertklik@gmail.com>
2020-09-29 23:18:14 -0300

    finishing migrations with table relations

---
+++ Server/WebCancelationTestServer/database/migrations/2020_09_30_020332_create_schools_to_students.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchoolsToStudents extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->foreignId("schools_id")
+                    ->constrained()
+                    ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->dropForeign(["schools_id"]);
+        });
+    }
+}
