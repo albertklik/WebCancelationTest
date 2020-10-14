6c19feca861577764cd6b1723051fd7ac388a607
Albertklik <paulo.albertklik@gmail.com>
2020-10-13 22:29:48 -0300

    Setting up first models application

---
+++ Server/WebCancelationTestServer/app/Models/Researches.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Researches extends Model
+{
+    use HasFactory;
+
+    /**
+     *  get the user associated with the research
+     */
+
+     public function user()
+     {
+         return $this->belongsTo(Users::class);
+     }
+
+    /**
+     * get the schools of the research.
+     */
+
+    public function schools()
+    {
+        return $this->hasMany(Schools::class);
+    }
+
+    /**
+     * get the thest groups of the reseach
+     */
+
+    public function testGroups()
+    {
+        return $this->hasMany(TestGroups::class);
+    }
+}
