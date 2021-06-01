318f9026e77562529976de2df58f88dee96fb632
Albertklik <paulo.albertklik@gmail.com>
2021-06-01 01:37:46 -0300

    creating and testing the 'tests' CRUD

--- Server/WebCancelationTestServer/app/Http/Controllers/TestsController.php
+++ Server/WebCancelationTestServer/app/Http/Controllers/TestsController.php
@@ -6,7 +6,47 @@ use Illuminate\Http\Request;
 use App\Models\Tests;
 class TestsController extends Controller
 {
-    public function show($id) {
+    public function index() 
+    {
+        $tests = Tests::all();
+        return response()->json($tests);
+    }
+
+    public function show($id) 
+    {
         return Tests::findOrFail($id);
+    }
+
+    public function store(Request $request) 
+    {
+        $validated = $request->validate(Tests::getValidationArray());
+        
+        $test = new Tests();
+        $test->fill($request->all());
+        $test->save();
+
+        return response()->json($test, 201);
+    }
+
+    public function update(Request $request, $id) {
+
+        $test = Tests::findOrFail($id);
+
+        $validated = $request->validate(Tests::getValidationArray());
+
+        $test->fill($request->all());
+        $test->save();
+
+        return response()->json($test);
+    }
+
+    public function destroy($id) 
+    {
+        $test = Tests::findOrFail($id);
+
+        if ($test->delete()) 
+        {
+            return response()->json($test);
+        }
     }
 }
--- Server/WebCancelationTestServer/app/Models/Tests.php
+++ Server/WebCancelationTestServer/app/Models/Tests.php
@@ -7,5 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tests extends Model
 {
+    protected $fillable = ['result','seconds','hits','misses','students_id','test_groups_id'];
     use HasFactory;
+
+    public static function getValidationArray() {
+        return [
+            'result' => 'required|json',
+            'seconds' => 'required|numeric',
+            'hits' => 'required|numeric',
+            'misses' => 'required|numeric',
+            'students_id' => 'required|numeric',
+            'test_groups_id' => 'required|numeric'
+        ];
+    }
 }
