68e782a0ce082c8689013bfc55a2e4aad8c9793c
Albertklik <paulo.albertklik@gmail.com>
2022-06-26 16:55:04 -0300

    publish updates

--- Server/publish/project/routes/api.php
+++ Server/publish/project/routes/api.php
@@ -24,12 +24,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/test/list', [TestController::class,'list'])->name('test.list');
+Route::post('/test/update/{test}', [TestController::class,'update'])->name('test.update');
+Route::post('/test/delete/{test}', [TestController::class,'destroy'])->name('test.delete');
 Route::apiResource('/test', TestController::class);
 
 Route::get('/testGroup/list', [TestGroupController::class,'list'])->name('testGroup.list');
+Route::post('/testGroup/update/{testGroup}', [TestGroupController::class,'update'])->name('testGroup.update');
+Route::post('/testGroup/delete/{testGroup}', [TestGroupController::class,'destroy'])->name('testGroup.delete');
 Route::apiResource('/testGroup', TestGroupController::class);
 
 Route::get('/research/list', [ResearchController::class,'list'])->name('research.list');
+Route::post('/research/update/{research}', [ResearchController::class,'update'])->name('research.update');
+Route::post('/research/delete/{research}', [ResearchController::class,'destroy'])->name('research.delete');
 Route::apiResource('/research', ResearchController::class);
 
 Route::post('/student/exists', [StudentController::class,'exists'])->name('student.exists');
